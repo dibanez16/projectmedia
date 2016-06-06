d927b3fc6342eeabf5bbf33c49b541b8f22a0da9
dibanez16 <diego@diegoibanez.net>
2016-06-05 22:16:53 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WiDi Design</title>
+    
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Web Design, Online Marketing">
+    <meta name="description" content="Online marketing agency and web design">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <style>
+        body {background-color:#152036;}
+        h1 {
+        font-family: "proxima-nova", sans-serif; font-weight: 100;
+            font-size:60px; color:#f19413;
+        }
+        p { color:#fff; font-family: "proxima-nova", sans-serif; font-weight: 100; 
+            line-height:18px; font-size:14px; margin:0;
+        }
+        .centered { height:100px; width:100%; position:absolute; top:50%;
+          left:0; margin-top:-50px; /* half the height of the element */ text-align:center;
+        }
+        
+        .bold { font-weight:700; }
+    </style>
+</head>
+    
+<body>
+    
+<div class="centered">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+                <img src="_shared/logo/logo-wididesign-whitetxt.png" width="400" height="auto" 
+                     class="img-responsive center-block">
+                <p>Online Marketing Agency</p>
+            </div>
+        </div>
+    </div>
+</div>
+                
+    
+    
+    
+<script src="https://use.typekit.net/tno0qbz.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!--
+<script src="https://cdn.jsdelivr.net/jquery/2.2.3/jquery.min.js"></script>    
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>    
+-->
+    
+<!--Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-76741604-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+    
+</body>
+    
+</html>
